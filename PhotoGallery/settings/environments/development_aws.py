e9d76be3033c1f00664846f7693d8b966b65b992
cosmin <cosmin@home.net>
2019-03-03 17:12:47 +0100

    change settings for dev aws to take into account a new test bucket

--- PhotoGallery/settings/environments/development_aws.py
+++ PhotoGallery/settings/environments/development_aws.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
+from PhotoGallery.settings.components.database import DATABASES_DEV
 from PhotoGallery.settings.utils.utils import parse_db_variable
 
 DEBUG = True
@@ -27,23 +28,11 @@ AWS S3 Storage settings
 """
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME_TEST')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 
-"""
-Database Postgres for Heroku
-"""
-db_variables = parse_db_variable(os.environ.get('DATABASE_URL'))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': db_variables[4],
-        'USER': db_variables[0],
-        'PASSWORD': db_variables[1],
-        'HOST': db_variables[2],
-        'PORT': db_variables[3]
-    }
-}
+
+DATABASES = DATABASES_DEV
 
 S3_URL = 'https://{0}.s3.amazonaws.com'.format(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 MEDIA_URL = S3_URL + "/photos/"
