e3c28f26e5614ecb79d047b828ed16147603a7a1
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-03-26 14:31:57 +0100

    use widgets to generate html form fields

--- PhotoGallery/forms/add_album_form.py
+++ PhotoGallery/forms/add_album_form.py
@@ -4,6 +4,14 @@ from django import forms
 
 
 class AddAlbumForm(forms.Form):
-    album_name = forms.CharField(label='Album name', max_length=100)
-    album_date = forms.DateField(input_formats=['%d-%m-%Y', '%d/%m/%Y'], initial=datetime.now().strftime('%d-%m-%Y'))
-    album_category = forms.CharField(label='Album category', max_length=100, initial='Photos')
+    album_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                               "placeholder": "Enter name"}),
+                                 label='Album name',
+                                 max_length=100)
+    album_date = forms.DateField(widget=forms.DateInput(attrs={"class": "form-control"}),
+                                 input_formats=['%d-%m-%Y', '%d/%m/%Y'],
+                                 initial=datetime.now().strftime('%d-%m-%Y'))
+    album_category = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                                   "placeholder": "Enter category"}),
+                                     label='Album category',
+                                     max_length=100, initial='Photos')
