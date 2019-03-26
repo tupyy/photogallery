42ffc0607d6b9eaccb4fb76e184d66ac58aad907
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-03-26 12:37:32 +0100

    add link into navbar to add album form

--- PhotoGallery/views/index.py
+++ PhotoGallery/views/index.py
@@ -4,11 +4,23 @@ from django.conf import settings
 from gallery.views import GalleryIndexView
 
 
-class PreviewGalleryIndexView(GalleryIndexView):
+class PreviewCommonMixin(object):
+    """ Provide can_add_album can_add_photo """
+    def can_add_album(self):
+        return self.request.user.has_perm('gallery.add_album')
+
+    def can_add_photo(self):
+        return self.request.user.has_perm('gallery.add_photo')
+
+
+class PreviewGalleryIndexView(PreviewCommonMixin, GalleryIndexView):
 
     def get_context_data(self, **kwargs):
         context = super(PreviewGalleryIndexView,self).get_context_data(**kwargs)
         albums = context['object_list']
+
+        if self.can_add_album():
+            context['can_add_album'] = True
 
         preview_year = {}
         albums_count = getattr(settings, 'GALLERY_YEAR_PREVIEW', 5)
