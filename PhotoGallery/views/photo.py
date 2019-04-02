b8c8ee88cc270aa9dda4f6bd2a77742641de7ba6
cosmin <cosmin@home.net>
2019-04-02 22:07:58 +0200

    add delete photo

--- PhotoGallery/views/photo.py
+++ PhotoGallery/views/photo.py
@@ -1,14 +1,17 @@
-from django.views.generic import DetailView
+from django.shortcuts import redirect
+from django.views.generic import DeleteView
 
 from gallery.models import Photo
 from gallery.storages import get_storage
 
 
-class DeletePhotoView(DetailView):
+class DeletePhotoView(DeleteView):
     model = Photo
+    template_name = 'photo_gallery/delete_photo_confirmation.html'
 
-    def post(self):
+    def post(self, request, *args, **kwargs):
         photo = self.get_object()
+        album_id = photo.album.id
 
         photo_storage = get_storage('photo')
         cache_storage = get_storage('cache')
@@ -17,3 +20,4 @@ class DeletePhotoView(DetailView):
         cache_storage.delete(photo.thumbnail('thumb'))
 
         photo.delete()
+        return redirect('/album/{}'.format(album_id))
